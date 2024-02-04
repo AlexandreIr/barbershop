b79f3a775edbe1b3b95d48c60d5eb23db7c76f01
Alexandre Fernandes <alex.silva250@hotmail.com>
2024-02-03 22:26:50 -0300

    feat: add initial search components

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,6 +1,7 @@
 import {format} from "date-fns";
 import Header from "../_components/header";
 import { ptBR } from "date-fns/locale";
+import Search from "./_components/search";
 
 export default function Home() {
     
@@ -14,6 +15,9 @@ export default function Home() {
                 {format(new Date(), "EEEE', 'dd ' de ' MMMM' de ' yyyy" , {
                 locale: ptBR
             })}</p>
+            </div>
+            <div className="p-5 mt-6">
+                <Search/>
             </div>
         </div>
     
