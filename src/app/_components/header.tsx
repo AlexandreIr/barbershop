e84dbd19b685730a84407dcd9b5dd065d8968208
Alexandre Fernandes <alex.silva250@hotmail.com>
2024-02-03 22:05:06 -0300

    feat: add welcome message

--- src/app/_components/header.tsx
+++ src/app/_components/header.tsx
@@ -7,7 +7,7 @@ const Header = () => {
     return ( 
         <Card>
             <CardContent className="p-5 justify-between flex flex-row items-center">
-            <Image src="/logo.png" alt="logo" height={22} width={120} />
+            <Image src="/logo.png" alt="logo" height={20} width={120} />
             <Button variant="outline" size="icon" className="h-6 w-6">
                 <MenuIcon size={16}/>
             </Button>
